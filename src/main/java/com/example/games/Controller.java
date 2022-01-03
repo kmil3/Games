3322531788105d9625c86b271d096491c46cbb70
kmil3 <weber.camile@gmail.com>
2022-01-03 10:00:16 -0300

    camiw

--- src/main/java/com/example/games/Controller.java
+++ src/main/java/com/example/games/Controller.java
@@ -1,5 +1,37 @@
 package com.example.games;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    TextField text_nome, text_desenvol;
+
+    @FXML
+    RadioButton btn_moba, btn_rpg, btn_puzzle, btn_fps;
+
+    @FXML
+    DatePicker data;
+
+    public void cadastrar(){
+        String nome = text_nome.getText();
+        String desenvol = text_desenvol.getText();
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
