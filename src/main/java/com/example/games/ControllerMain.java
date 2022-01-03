126565bd73af8d9f754e5e15867f09069dd179a8
kmil3 <weber.camile@gmail.com>
2022-01-03 16:11:36 -0300

    camiw

---
+++ src/main/java/com/example/games/ControllerMain.java
@@ -0,0 +1,8 @@
+package com.example.games;
+
+public class ControllerMain {
+    public void cadastrar(){}
+    public void visualizar(){}
+    public void alterar(){}
+    public void remover(){}
+}
