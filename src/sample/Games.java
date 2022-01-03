522fbddcab9a36186b87457d1a07d267105ecc1e
kmil3 <weber.camile@gmail.com>
2022-01-03 08:09:46 -0300

    camiw

--- src/sample/Games.java
+++ src/sample/Games.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class Games {
-    String nome, genero, empresa;
+    String nome, genero, desenvolvedor;
     int ano;
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("JANELA_CADASTRO.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("cadastro.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 427));
         primaryStage.show();
