7707cdd7dfe6253c21ebb9c314054b029ec088c3
JadeBrummelhaus <jadinabrummelhaus@gmail.com>
2022-01-05 19:22:37 -0300

    jade

--- src/main/java/sample/ControllerRemover.java
+++ src/main/java/sample/ControllerRemover.java
@@ -42,7 +42,7 @@ public class ControllerRemover{
                 i++;
             }
             if (posicao == -1){
-                Alert dialogo = new Alert(Alert.AlertType.ERROR, "Ficha não encontrada!");
+                Alert dialogo = new Alert(Alert.AlertType.ERROR, "Game não encontrada!");
                 dialogo.showAndWait();
             } else{
                 listaDeGames.remove(posicao);
